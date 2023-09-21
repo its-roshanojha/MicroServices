deca2ff991893b922ce5e6365b753728f54673a0
roshan <roshan@samarth.ac.in>
2023-09-21 17:33:29 +0530

    Inital Commit

---
+++ header-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; // Must be imported for webpack to work
+import './App.css';
+
+function App() {
+  return (
+    <div className="HeaderApp">
+      <div>HELLO WE HAVE INITIATED THE MICRO SERVICES</div>
+    </div>
+  );
+}
+
+export default App;
